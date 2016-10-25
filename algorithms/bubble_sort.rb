e46d787e50d6e077d81d0566ae1de529dbbc9398
giovani <giovani@DEV-imac3.local>
2016-10-25 11:05:51 -0700

    adds pictureS

--- algorithms/bubble_sort.rb
+++ algorithms/bubble_sort.rb
@@ -48,32 +48,26 @@
 # p bubble_sort(arr)
 
 
+# def bubble_sort(array)
+# 	n = array.length
 
+# 	loop do 
+# 		swapped = false
+# 		(n-1).times do |i|
+# 			if array[i] > array [i + 1]
+# 				array[i], array[i+1] = array[i+1], array[i]
+# 			swapped= true
+# 			end
+# 		end
+# 		break if not swapped
+# 	end
 
+# 	array
+# end
 
+# arrr=[324,45,2,3,4532,2,1,4,32,1]
 
-
-
-def bubble_sort(array)
-	n = array.length
-
-	loop do 
-		swapped = false
-		(n-1).times do |i|
-			if array[i] > array [i + 1]
-				array[i], array[i+1] = array[i+1], array[i]
-			swapped= true
-			end
-		end
-		break if not swapped
-	end
-
-	array
-end
-
-arrr=[324,45,2,3,4532,2,1,4,32,1]
-
-p bubble_sort(arrr)
+# p bubble_sort(arrr)
 
 
 
--- algorithms/quicksort.rb
+++ algorithms/quicksort.rb
@@ -1,28 +1,28 @@
-class Array
-def quicksort
+# class Array
+# def quicksort
 
-	return [] if empty?
-	#[23,423,32,1,2,4,2,1,5]
+# 	return [] if empty?
+# 	#[23,423,32,1,2,4,2,1,5]
 
-	#[23,423,32, ,2,4,2,1,5]
+# 	#[23,423,32, ,2,4,2,1,5]
 
-	#pivot = 1
-	#left = 
-	#right
+# 	#pivot = 1
+# 	#left = 
+# 	#right
 
-	pivot = delete_at(rand(size))
+# 	pivot = delete_at(rand(size))
 
-	left, right = partition(&pivot.method(:>))
+# 	left, right = partition(&pivot.method(:>))
 
-	return *left.quicksort, pivot, *right.quicksort
+# 	return *left.quicksort, pivot, *right.quicksort
 
 
-end
-end
+# end
+# end
 
-arr = [23,423,32,1,2,4,2,1,5]
+# arr = [23,423,32,1,2,4,2,1,5]
 
-p arr.quicksort
+# p arr.quicksort
 
 
 # class Array
@@ -47,3 +47,76 @@ p arr.quicksort
 # arr = [23,5,45,2,3434,56,34,453,1,2,43,2]
 
 # p arr.quicksort
+
+
+
+class Array
+	def quicksort
+		#[34,2,12,3,23,4,5,1,234324,3243]
+		  
+
+		#pivot = 4
+
+		pivot = delete_at(rand(size))
+
+		#left= [34,2,12,3,23]
+
+		#right= [5,1,234324,3243]
+
+left, right = partition(&pivot.method(:>))
+
+return *left.quicksort, pivot, *right.quicksort
+
+
+	end
+end
+
+a = [34,2,12,3,23,4,5,1,234324,3243]
+
+ a.quicksort
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
